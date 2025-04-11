7f1355990f4c836ca5ed49b397ce5f1938bda30e
deplagene <deplagene@mail.ru>
2025-04-12 00:10:23 +0300

    доработал основной gui

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,20 @@
 package utils
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/theme"
+)
+
+// todo: добавить иконки
+func themeIconsMustLoad(path string) fyne.Resource {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Println("Icon load error:", err)
+		return theme.FyneLogo()
+	}
+	return fyne.NewStaticResource(filepath.Base(path), data)
+}
