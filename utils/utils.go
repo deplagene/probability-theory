30af636eb43b65950efc84959ef617d9ab37e1a5
Anna Anikeva <blackfyd@MBP-Anna.Dlink>
2025-04-12 19:07:07 +0300

    добавила все изменения

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,19 @@
 package utils
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/theme"
+)
+
+func ThemeIconsMustLoad(path string) fyne.Resource {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Println("Icon load error:", err)
+		return theme.FyneLogo()
+	}
+	return fyne.NewStaticResource(filepath.Base(path), data)
+}
